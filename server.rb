6296f7446bac90b90ff8f6f28f325d220ff94325
Zarkiel <zarkiel@gmail.com>
2023-12-18 10:17:04 -0500

    Update server.rb
    Changed response to json and added rescuer

--- server.rb
+++ server.rb
@@ -3,9 +3,16 @@ require 'json'
 require './shugo'
 
 set :default_content_type, :json
+set :port, 4567
 
 post '/' do
-    shugo = Zarkiel::Shugo.new(request)
-    shugo.deliver
-    shugo.relay
+    begin
+        shugo = Zarkiel::Shugo.new(request)
+        shugo.deliver
+        shugo.relay
+
+        JSON.generate({status: "OK"})
+    rescue => error
+        JSON.generate({status: "ERROR", message: error.message})
+    end    
 end
